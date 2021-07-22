41f2417fd99e4ee67d01b3930220896aea0c14b5
trufflesmld <trufflesmld@gmail.com>
2021-07-22 19:53:11 +0300

    init SideBar

---
+++ src/components/SideBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="sideBar">1111</div>
+</template>
+
+<style scoped>
+.sideBar {
+    position: fixed;
+    width: 25vw;
+    height: 92vh;
+    border-right: solid 1px #d8d8d8 ;
+    margin:0 ;
+    background-color: chartreuse;
+}
+</style>
--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="container">
     <NavBar />
+    <div class="main">
+      <SideBar />
+      <div class="list"></div>
+    </div>
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar.vue";
+import SideBar from "@/components/SideBar.vue";
 
 export default {
   components: {
     NavBar,
+    SideBar,
   },
 };
 </script>
@@ -18,5 +24,22 @@ export default {
 .container {
   margin: 0;
   padding: 0;
+}
+.main {
+  z-index: -1;
+  position: relative;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  top: 8vh;
+  width: 80vw;
+  height: auto;
+}
+.list {
+  position: relative;
+  left: 25vw;
+  display: block;
+  width: 55vw;
+
 }
 </style>
